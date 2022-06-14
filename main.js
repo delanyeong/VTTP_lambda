51cef18f5b39de01e4fc3d0cfa8e1c6fbec67672
delanyeong <delan.april@gmail.com>
2022-06-14 09:59:07 +0800

    some code

---
+++ main.js
@@ -0,0 +1,12 @@
+function greetings(name) {
+  console.info(`Hello ${name}`);
+  //System.out.printf("Hello %s, name");
+}
+console.info("hello, world");
+greetings('fred');
+greetings('barney');
+greetings(3.142);
+
+//duck typing
+//behaves like a duck it must be a duck
+
